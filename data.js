1c6953898e3cf569a4f4990905d3de2e4b58d9c5
Nikhitha999-nikki <nikhithamalapula@gmail.com>
2025-03-23 21:57:55 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+
+    let movies = [
+        {
+            name: 'Jhansi',
+            des: 'The story of a female cop who has past and gets reflect in her ongoing life and series then there is human trafficking where she is also a victim and how she takes revenge again...',
+            image: 'slider-1.png'
+        },
+        {
+            name: 'The Warrior',
+            des: '"The Warriorr" is described as a Telugu action crime movie where Dr. Satya, tormented by a medical mafia kingpin, Guru, returns as a powerful police officer to end Guru\'s reign of terror',
+            image: 'slider-2.png'
+        },
+        {
+            name: 'Exposed',
+            des: '"Exposed" on Hotstar is a web series about Greeshma, an acclaimed news anchor, who faces a challenge from a rising star with ulterior motives, causing ripples within her organization',
+            image: 'slider-3.png'
+        },
+        {
+            name: 'Vikram Vedha',
+            des: '"Vikram Vedha" is described as a Hindi thriller where a police officer, Vikram, confronts a dreaded gangster, Vedha, who engages him in a mind game, challenging Vikram\'s perception of good and evil',
+            image: 'slider-4.png'
+        },
+        {
+            name: 'Bheemla Nayak',
+            des: 'An upright cop goes toe to toe with an ex-havildar over a petty case',
+            image: 'slider-5.png'
+        }
+    ];
+
